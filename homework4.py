d9a510b2139ab5954bfacfad7cfc591c06c473b7
malvvik <malvvik@gmail.com>
2024-06-06 16:39:44 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,11 @@
+# my_string=input("Привет, какой сегодня день? ")
+# print(len(my_string))
+# print("Привет, какой сегодня день".upper())
+# print("Привет, какой сегодня день".lower())
+print("Привет, какой сегодня день?".replace(" ", ""))
+my_string="Привет какой сегодня день"
+print(my_string[0])
+print(my_string[-1])
+
+
+
